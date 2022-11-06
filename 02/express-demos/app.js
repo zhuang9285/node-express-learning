cd0d0483d6ae99aec4fecf688251c4d80f2e2958
zhuang <coderzhuang@163.com>
2022-11-06 21:21:39 +0800

    epxress 体验

--- 02/express-demos/app.js
+++ 02/express-demos/app.js
@@ -3,8 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World get /')
 })
+app.post('/', (req, res) => {
+  res.send('post /')
+})
+
+app.get('/user', (req, res) => {
+  res.send('get /user')
+})
+app.post('/user', (req, res) => {
+  res.send('post /user')
+})
+app.put('/user', (req, res) => {
+  res.send('put /user')
+})
+app.delete('/user', (req, res) => {
+  res.send('delete /user')
+})
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log(`Server running at http://localhost:3000/`)
